6ce19d327c86e4f046e07ceb9307c0490a84ff14
Eduardo Oviedo-Blanco <eduardo.oviedo@gmail.com>
2022-10-24 11:45:21 -0600

    Workshop ready, completed

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("C# Loops!");
+
+Console.WriteLine("Numbers from 0 to 1000...");
+
+int i = 0;
+
+while(i <= 1000) {
+  Console.Write(i + " ");
+  i = i + 1;
+}
+
+Console.WriteLine("Completed\n\n");
+
+Console.WriteLine("Even numbers");
+
+int from;
+int to;
+string? input;
+
+Console.Write("Enter the first number: ");
+input = Console.ReadLine();
+from = Convert.ToInt32(input);
+Console.Write("Enter the second number: ");
+input = Console.ReadLine();
+to = Convert.ToInt32(input);
+
+for (int y = from; y < to; y++)
+{
+  if (y % 2 == 0){
+    Console.Write(y + " ");
+  }
+}
+
+Console.WriteLine("End of execution");
